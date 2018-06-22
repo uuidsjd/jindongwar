af3d9c2ddf6afa1bdb3e834b6dad600906b574e8
uuidsjd <252619994@qq.com>
2018-06-22 15:47:10 +0800

    [add]新增https连接方式（忽略证书）

---
+++ src/main/java/com/test/httpUtil/HttpClientUtil.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package com.test.httpUtil;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.util.EntityUtils;
+
+
+/**
+ * @author 晋东
+ *
+ */
+public class HttpClientUtil {
+
+	public static String doPost(String url,String jsonStr,String charset){
+		HttpClient httpClient = null;
+		HttpPost httpPost = null;
+		String result = null;
+		try {
+			httpClient = new SSLClient();
+			httpPost = new HttpPost(url);
+			httpPost.addHeader("Content-Type","application/json");
+			StringEntity se = new StringEntity(jsonStr);
+			se.setContentType("text/json");
+			se.setContentEncoding(new BasicHeader("Content-Type", "application/json"));
+			httpPost.setEntity(se);
+			HttpResponse response = httpClient.execute(httpPost);
+			if(response!=null){
+				HttpEntity resEntity = response.getEntity();
+				if(response!=null)
+					result = EntityUtils.toString(resEntity, charset);
+			}
+		} catch (Exception e) {
+			//http构建失败
+			e.printStackTrace();
+		}
+		return result;
+		
+	}
+	
+	public static void main(String[] args) {
+		String url = "https://baidu.com";
+		String str = HttpClientUtil.doPost(url, "zhongguo", "utf-8");
+		System.out.println(str);
+	}
+	
+	
+}
